117a560d1383066d0d90acf2cf643c374d41a0ed
huysyyrps <huxf15269179758@162.com>
2020-06-23 16:14:02 +0800

    2020-06-23

--- app/src/main/java/main/utils/utils/JudgePhone.java
+++ app/src/main/java/main/utils/utils/JudgePhone.java
@@ -4,7 +4,7 @@ import android.text.TextUtils;
 
 public class JudgePhone {
     public boolean isMobileNO(String mobiles) {
-        String telRegex = "^((1[3,5,7,8,9][0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+        String telRegex = "^((1[3,5,6,7,8,9][0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
         if (TextUtils.isEmpty(mobiles)) {
             return false;
         } else {
